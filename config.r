9956610f61f7814e05cf651ad5741fd5fce1d8a9
andrecatini@gmail.com <andrecatini@gmail.com@4c3250b1-33cf-aad5-bd2c-e2d7e36ee312>
2014-09-19 05:40:56 +0000

    Comentários e correção de diretórios

--- config.r
+++ config.r
@@ -1,6 +1,7 @@
 #install.packages("ape");
 #install.packages("e1071");
 #install.packages("multicore");
+#install.packages("class");
 #install.packages("randomForest");
 #install.packages("rpart");
 #install.packages("RWeka");
@@ -8,7 +9,8 @@
 # Packages
 library(ape)
 library(e1071);
-library(multicore);
+library(class);
+#library(multicore);
 library(randomForest);
 library(rpart); 
 library(RWeka);
@@ -23,7 +25,7 @@ RATES = c(0.05, 0.1, 0.2, 0.4);
 EPOCHS = 10;
 
 # Directory
-DIR = ("/home/andrecatini/");
+DIR = (path.expand("~/"));
 
 # Complexity metrics
 METRICS = c("F1", "F1v", "F2", "F3", "F4", "L1", "L2", "L3", "N1", "N2", "N3", "N4", "T1", "T2");
@@ -42,18 +44,19 @@ MLP = make_Weka_classifier("weka/classifiers/functions/MultilayerPerceptron");
 # Weka Nominal to binary
 NTB = make_Weka_filter("weka/filters/supervised/attribute/NominalToBinary");
 
-Directories
-setwd("/home/andrecatini/ic-alcparo/");
+#Directories
+setwd(path.expand("~/ic-alcparo/"));
 
 # Files
-FILES.NAMES = list.files("/home/andrecatini/Dropbox/LINUX/Datasets_processados/");
-FILES = paste("/home/andrecatini/Dropbox/LINUX/Datasets_processados/", FILES.NAMES, sep="");
+#FILES.NAMES = list.files(path.expand("~/Datasets_processados/"));
+FILES.NAMES = as.matrix(read.table("files.bases.ok.names.txt"))[,1];
+FILES = paste(path.expand("~/Datasets_processados/"), FILES.NAMES, sep="");
 
 DBL_MAX = .Machine$double.xmax;
 DBL_MIN = .Machine$double.xmin;
 
 # Metabases file names
-FILES.METABASES = paste("/home/andrecatini/metabases/", list.files("/home/andrecatini/metabases/"), sep="");
+FILES.METABASES = paste(path.expand("~/metabases/"), list.files(path.expand("~/metabases/")), sep="");
 
 # Number of regressors iterations
 EXAMPLES = 5; #2 ou mais
@@ -67,5 +70,12 @@ source("measures.r");
 source("metabase.r");
 source("regressors/regressors.r");
 
-#METABASE.FILES.NAMES = list.files("/home/andrecatini/metabases/");
-#METABASE.FILES = paste("/home/andrecatini/metabases/", METABASE.FILES.NAMES, sep="");
+
+#FILES.METABASES.NAMES = list.files(path.expand("~/metabases/"));
+#FILES.METABASES.NAMES = strsplit(FILES.METABASES.NAMES, split=".metabase");
+#FILES.METABASES.NAMES = do.call(rbind, FILES.METABASES.NAMES);
+
+#FILES.BASES.OK.NAMES = intersect(FILES.NAMES,FILES.METABASES.NAMES);
+#FILES.BASES.OK = paste(path.expand("~/Datasets_processados/"), FILES.BASES.OK.NAMES, sep="");
+#write(FILES.BASES.OK.NAMES, "files.bases.ok.names.txt");
+
--- run.r
+++
@@ -1,6 +0,0 @@
-setwd("/home/andrecatini/ic-alcparo/");
-source("config.r");
-
-#audiology.arff removido. Poucos exemplos
-#abalone.arff deu erro.
-#flags.arff : erro no randomForest
